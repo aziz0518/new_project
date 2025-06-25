671889fdd2b01187c00bf1050dce8ba10c293d5c
aziz0518 <158218044+aziz0518@users.noreply.github.com>
2025-06-25 17:38:35 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -7,7 +7,6 @@ from .serializers import RegisterSerializer
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'message': 'Foydalanuvchi yaratildi'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'message': 'Foydalanuvchi yaratildi'}, status=status.HTTP_201_CREATED)
