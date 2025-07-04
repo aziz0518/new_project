f5b96d8b1b2390f05c9f1552ac3b290cdc55089c
aziz0518 <158218044+aziz0518@users.noreply.github.com>
2025-07-04 15:41:42 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterView(APIView):
     def post(self, request):
@@ -10,3 +13,32 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({'message': 'Foydalanuvchi yaratildi'}, status=status.HTTP_201_CREATED)
+# views.py
+
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response(
+                {"error": "Username va parolni kiriting"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"error": "Login yoki parol noto‘g‘ri"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
